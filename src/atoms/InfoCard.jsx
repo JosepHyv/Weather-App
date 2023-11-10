c162cdea611bd749b1501f1bdb64fe3ae91a7fd7
josephyv <haguilar@auronix.com>
2023-11-09 19:05:57 -0600

    Refactorizando tarjetas de informacion

--- src/atoms/InfoCard.jsx
+++ src/atoms/InfoCard.jsx
@@ -7,7 +7,7 @@ const Card = ({
   description = "  ",
 }) => {
   return (
-    <div class="flex flex-col flex-wrap h-60 w-60 rounded-2xl justify-start bg-opacity-30 bg-gray-700 border-gray-400 shadow-md border-opacity-10 backdrop-blur-md text-white px-10 py-5">
+    <div class="flex flex-col flex-wrap h-60 w-6j0 rounded-2xl justify-start bg-opacity-30 bg-gray-700 border-gray-400 shadow-md border-opacity-10 backdrop-blur-md text-white px-10 py-5">
       <p class="border text-2xl capitalize drop-shadow-2xl text-blue-50 text-opacity-50 font-medium">
         {Title}
       </p>
